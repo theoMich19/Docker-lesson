823de15fa3aa104aa2581f68a7ea1c59f93a26d4
theoMich <michellontheo.pro@gmail.com>
2023-11-05 20:34:29 +0100

    add projet docker configuration

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Bienvenu jeune baleno, prépare toi pour de nouvelles aventures
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
